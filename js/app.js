db0eecc2c4148e4a7c5a20e6cdbf9e3ff0a69eb2
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-05-02 11:43:09 +0700

    ded

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$('.autoplay').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+});
+
+var $slickElement = $('.autoplay');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    // $status.text(i + '/' + slick.slideCount);
+    $(".now").text(i);
+    $(".totalx").text(slick.slideCount)
+});
